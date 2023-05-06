1b7101f97146fe7f65a687bd2f72a0636e296c5e
Malek-Abdelal <malekbassam1144@gmail.com>
2023-05-07 00:45:36 +0300

    Array reverse challenge is done

---
+++ array-reverse/README.md
@@ -0,0 +1,7 @@
+# Reverse an Array
+
+a function that takes an array and returns the reversed one.
+
+## Whiteboard Process
+![Whiteboard](./Whiteboard.png)
+
---
+++ array-reverse/array_reverse.py
@@ -0,0 +1,8 @@
+
+array = ['a', 'b', 'c', 'd', 'e']
+
+def reverseArray(arr):
+    reversed_arr = arr[::-1]
+    return reversed_arr
+
+print(reverseArray(array))
