54a0f0ac59fd919d13fc6056f4a9ba97242b8b0c
Malek-Abdelal <malekbassam1144@gmail.com>
2023-05-14 01:19:18 +0300

    Challenge is done

--- linked-list/linked_list/linked_list.py
+++ linked-list/linked_list/linked_list.py
@@ -5,15 +5,15 @@ class Node :
         self.next = next
 
 class LinkedList:
-    counter = 0
+    # counter = 0
     def __init__(self, head = None):
         self.head = head
 
 
     def insert(self, value):
-        if self.head == None:
-            LinkedList.counter += 1
-        LinkedList.counter += 1
+        # if self.head == None:
+        #     LinkedList.counter += 1
+        # LinkedList.counter += 1
         new_node = Node(value)
         new_node.next = self.head
         self.head = new_node
@@ -39,12 +39,8 @@ class LinkedList:
 if __name__ == "__main__":
     f_node = Node(1)
     f_linked_list = LinkedList()
-    # print(f_linked_list.head)
     print(LinkedList.counter)
-    f_linked_list.insert(2)
-    f_linked_list.insert(2)
     f_linked_list.insert(2)
     print(LinkedList.counter)
     print(f_linked_list.to_string())
-    # print(f_linked_list.head)
-    # print(f_node.value)
+   
